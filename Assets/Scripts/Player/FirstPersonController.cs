f5d6902ada719bbbb066959a906ede9bddf7b1e1
Jack <brown.jt.dev@gmail.com>
2025-10-14 21:40:49 +0100

    Basic player implementation

---
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    #region Editor Variables
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
+
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravityMultiplier = 1.0f;
+
+
+    [Header("Look Parameters")]
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float upDownLookRange = 80f;
+
+    [Header("References")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private PlayerInputHandler playerInputHandler;
+    //[SerializeField] private Animator animator;
+    #endregion
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+    private float CurrentSpeed => walkSpeed * (playerInputHandler.SprintTriggered ? sprintMultiplier : 1);
+
+    //private bool iFramesActive = false;
+    //private float iFramesTimer = 0.5f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // Ensure animator knows we are grounded and speed 0 at start
+        //animator.SetBool("IsGrounded", true);
+        //animator.SetFloat("Speed", 0f);
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    /// <summary>
+    /// Helper function to calculate world direction
+    /// </summary>
+    /// <returns>Normalized world direction</returns>
+    private Vector3 CalculateWorldDirection()
+    {
+        Vector3 inputDirection = new Vector3(playerInputHandler.MovementInput.x, 0f, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+
+    /// <summary>
+    /// Helper function to handle jumping used in HandleMovement
+    /// </summary>
+    private void HandleJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            //animator.SetBool("IsJumping", false);
+            currentMovement.y = -0.5f;
+            if (playerInputHandler.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+
+                // Also fire the animator Jump trigger
+                //animator.SetTrigger("Jump");
+            }
+        }
+        else
+        {
+            //animator.SetBool("IsJumping", true);
+            currentMovement.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Main function to handle movement of a player character
+    /// </summary>
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirection();
+        currentMovement.x = worldDirection.x * CurrentSpeed;
+        currentMovement.z = worldDirection.z * CurrentSpeed;
+
+        // Handle jumping as part of movement
+        HandleJumping();
+
+        characterController.Move(currentMovement * Time.deltaTime);
+
+        // Updating animator to match movement
+        // Ensuring speed is normalized and lerped to allow for smooth animation transitions
+        //float maxSpeed = walkSpeed * sprintMultiplier;
+        //float speed = new Vector3(currentMovement.x, 0f, currentMovement.z).magnitude;
+        //float normalizedSpeed = speed / maxSpeed;
+        //float lerpedSpeed = Mathf.Lerp(animator.GetFloat("Speed"), normalizedSpeed, Time.deltaTime * 10f);
+
+        //animator.SetFloat("Speed", lerpedSpeed);
+        //animator.SetBool("IsGrounded", characterController.isGrounded);
+    }
+
+    /// <summary>
+    /// Helper function to apply horizontal rotation
+    /// </summary>
+    /// <param name="rotationAmount">Amount of rotation to apply</param>
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+
+    /// <summary>
+    /// Helper function to apply vertical rotation
+    /// </summary>
+    /// <param name="rotationAmount">Amount of rotation to apply</param>
+    private void ApplyVerticalRotation(float rotationAmount)
+    {
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    /// <summary>
+    /// Main function to handle both horizontal and vertical rotations
+    /// </summary>
+    private void HandleRotation()
+    {
+        float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+        float mouseYRotation = playerInputHandler.RotationInput.y * mouseSensitivity;
+
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRotation(mouseYRotation);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    #region Editor Variables
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField] private string actionMapName = "Player";
+
+    [Header("Action Name References")]
+    [SerializeField] private string movement = "Move";
+    [SerializeField] private string rotation = "Look";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string sprint = "Sprint";
+    [SerializeField] private string interact = "Interact";
+    [SerializeField] private string attack = "Attack";
+    #endregion
+
+    private InputAction movementAction;
+    private InputAction rotationAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+    private InputAction interactAction;
+    private InputAction attackAction;
+
+    public Vector2 MovementInput { get; private set; }
+    public Vector2 RotationInput { get; private set; }
+    public bool JumpTriggered { get; private set; }
+    public bool SprintTriggered { get; private set; }
+    public bool InteractTriggered { get; private set; }
+    public bool AttackTriggered { get; private set; }
+
+    /// <summary>
+    /// When loading script we ensure we find the correct action map
+    /// as well as associated actions from it that we care about
+    /// </summary>
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        movementAction = mapReference.FindAction(movement);
+        rotationAction = mapReference.FindAction(rotation);
+        jumpAction = mapReference.FindAction(jump);
+        sprintAction = mapReference.FindAction(sprint);
+        interactAction = mapReference.FindAction(interact);
+        attackAction = mapReference.FindAction(attack);
+
+        SubscribeActionValuesToInputEvents();
+    }
+
+    private void Update()
+    {
+        // One shot attack
+        AttackTriggered = attackAction.WasPerformedThisFrame();
+
+        // One shot interact
+        InteractTriggered = interactAction.WasPerformedThisFrame();
+    }
+
+    /// <summary>
+    /// Function to subscribe all values to their respective input events
+    /// </summary>
+    private void SubscribeActionValuesToInputEvents()
+    {
+        movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+        movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
+
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
+        rotationAction.canceled += inputInfo => RotationInput = Vector2.zero;
+
+        jumpAction.performed += inputInfo => JumpTriggered = true;
+        jumpAction.canceled += inputInfo => JumpTriggered = false;
+
+        sprintAction.performed += inputInfo => SprintTriggered = true;
+        sprintAction.canceled += inputInfo => SprintTriggered = false;
+    }
+
+    /// <summary>
+    /// Helper function to enable the action map when required
+    /// </summary>
+    private void OnEnable()
+    {
+        playerControls.FindActionMap(actionMapName).Enable();
+    }
+
+    /// <summary>
+    /// Helper function to disable the action map when required
+    /// </summary>
+    private void OnDisable()
+    {
+        playerControls.FindActionMap(actionMapName).Disable();
+    }
+}
